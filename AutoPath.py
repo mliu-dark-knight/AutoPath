precision recall, need to fix bug

AutoPath.py
@@ -1,9 +1,12 @@
 import gc
-import numpy as np
+import operator
 from copy import deepcopy
-from NN import *
-from tqdm import tqdm
 from random import shuffle
+
+import numpy as np
+from tqdm import tqdm
+
+from NN import *
 
 
 class AutoPath(object):
@@ -13,7 +16,9 @@ class AutoPath(object):
 		self.build()
 
 	def build(self):
-		self.embedding = embedding('Embedding', [self.params.num_node, self.params.embed_dim])
+		self.training = tf.placeholder(tf.bool)
+		# last index is None embedding
+		self.embedding = embedding('Embedding', [self.params.num_node + 1, self.params.embed_dim])
 		self.indices = tf.placeholder(tf.int32, [None])
 		self.labels = tf.placeholder(tf.int32, [None])
 		self.neighbors = tf.placeholder(tf.int32, [None, None])
@@ -25,7 +30,7 @@ class AutoPath(object):
 		self.build_PPO()
 
 	def build_classification(self):
-		embedding = tf.nn.embedding_lookup(self.embedding, self.indices)
+		embedding = dropout(tf.nn.embedding_lookup(self.embedding, self.indices), self.params.keep_prob, self.training)
 		logits = fully_connected(embedding, self.params.num_type, 'Classification', activation='linear')
 		self.prediction = tf.argmax(logits, axis=1)
 		loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.one_hot(self.labels, self.params.num_type), logits=logits), axis=0)
@@ -96,25 +101,27 @@ class AutoPath(object):
 		return fully_connected(hidden, self.params.embed_dim, 'policy_o', activation='linear')
 
 	# the number of trajectories sampled is equal to batch size
-	def collect_trajectory(self, sess):
-		start_state = self.environment.initial_state()
+	def collect_trajectory(self, sess, start_state):
 		feed_state = deepcopy(start_state)
 		states, actions = [], []
 		for i in range(self.params.trajectory_length):
 			states.append(feed_state)
-			feed_neighor = self.environment.get_neighbors(feed_state[:, 0])
+			feed_neighbor = self.environment.get_neighbors(feed_state[:, 0])
 			# action contains indices of actual node IDs
 			action_indices = sess.run(self.decision,
-			                          feed_dict={self.state: feed_state, self.neighbors: feed_neighor})
-			action = feed_neighor[np.array(range(self.params.batch_size)), action_indices]
+			                          feed_dict={self.state: feed_state, self.neighbors: feed_neighbor})
+			action = feed_neighbor[np.array(range(len(start_state))), action_indices]
 			actions.append(action)
 			feed_state = self.environment.next_state(feed_state, action)
 		states = np.transpose(np.array(states), axes=(1, 0, 2)).tolist()
 		actions = np.transpose(np.array(actions)).tolist()
-		return self.environment.reward_multiprocessing(states, actions)
+		return states, actions
 
 	def PPO_epoch(self, sess):
-		states, actions, rewards = self.collect_trajectory(sess)
+		start_state = self.environment.initial_state()
+		states, actions = self.collect_trajectory(sess, start_state)
+		states, actions, rewards = self.environment.compute_reward(states, actions)
+		assert len(states) == len(actions) and len(actions) == len(rewards)
 		indices = range(self.params.trajectory_length * self.params.batch_size)
 		shuffle(indices)
 		batch_size = self.params.trajectory_length * self.params.batch_size / self.params.step
@@ -137,7 +144,7 @@ class AutoPath(object):
 	def classification_epoch(self, sess):
 		for _ in tqdm(range(self.params.outer_step), ncols=100):
 			indices, labels = self.sample_classification()
-			sess.run(self.classification_step, feed_dict={self.indices: indices, self.labels: labels})
+			sess.run(self.classification_step, feed_dict={self.indices: indices, self.labels: labels, self.training: True})
 
 	def train(self, sess):
 		sess.run(tf.global_variables_initializer())
@@ -151,7 +158,7 @@ class AutoPath(object):
 		for t in types:
 			labels += [self.environment.type_to_id[t]] * len(self.environment.type_to_node[t])
 			indices += list(self.environment.type_to_node[t])
-		predictions = sess.run(self.prediction, feed_dict={self.indices: np.array(indices)})
+		predictions = sess.run(self.prediction, feed_dict={self.indices: np.array(indices), self.training: False})
 		correct = 0
 		for label, prediction in zip(labels, predictions):
 			if label == prediction:
@@ -159,4 +166,20 @@ class AutoPath(object):
 		return float(correct) / len(indices)
 
 	def plan(self, sess):
-		pass
+		start_state = self.environment.initial_test()
+		actions = []
+		for i in range(int(math.ceil(len(start_state) / float(self.params.batch_size)))):
+			_, action = self.collect_trajectory(sess, start_state[i * self.params.batch_size : min((i + 1) * self.params.batch_size, len(start_state))])
+			actions.append(action)
+		actions = np.concatenate(actions, axis=0)
+		start_state = start_state[:, 0]
+
+		recommendation = {}
+		for state, action in zip(start_state, actions):
+			visited = {}
+			for a in action:
+				if a not in visited:
+					visited[a] = 0
+				visited[a] += 1
+			recommendation[state] = sorted(visited.items(), key=operator.itemgetter(1), reverse=True)
+		return recommendation

Environment.py
@@ -9,8 +9,8 @@ class Environment(object):
 		self.params = params
 		self.load_node()
 		self.load_graph()
-		self.train_pairs = self.load_pair(self.params.train_file)
-		self.test_pairs = self.load_pair(self.params.test_file)
+		self.train_data = self.load_train(self.params.train_files)
+		self.test_data = self.load_test(self.params.test_file)
 
 	def load_node(self):
 		self.id_to_name, self.name_to_id, self.type_to_node, self.id_to_type, self.type_to_id = \
@@ -29,20 +29,35 @@ class Environment(object):
 					data.append(1.0)
 		self.graph = csr_matrix((data, (row, col)), shape=(self.params.num_node, self.params.num_node))
 
-	def load_pair(self, path):
-		pairs = utils.load_pair(path)
-		return_pairs = []
-		for pair in pairs:
-			if pair[0] in self.name_to_id and pair[1] in self.name_to_id:
-				return_pairs.append(np.array((self.name_to_id[pair[0]], self.name_to_id[pair[1]])))
-		return np.array(return_pairs)
+	def load_train(self, paths):
+		groups = utils.load_groups(paths)
+		return [np.array([self.name_to_id[name] for name in group if name in self.name_to_id ]) for group in groups]
 
-	# returns a 2d array, 2nd dimension is 2
+	def load_test(self, path):
+		related = utils.load_pair(path)
+		return_related = {}
+		for key, values in related.items():
+			if key in self.name_to_id:
+				ids = set([self.name_to_id[value] for value in values if value in self.name_to_id])
+				if bool(ids):
+					return_related[self.name_to_id[key]] = ids
+		return return_related
+
+	# returns a batch of 2d array, 2nd dimension is 2
 	def initial_state(self):
-		return self.train_pairs[np.random.randint(len(self.train_pairs), size=self.params.batch_size)]
+		total_size = sum([len(group)**2 for group in self.train_data])
+		batch_size = self.params.batch_size
+		states = []
+		for group in self.train_data:
+			sample_size = max(len(group)**2 * batch_size / total_size, 1)
+			states.append(np.stack([np.random.choice(group, size=sample_size), np.random.choice(group, size=sample_size)], axis=1))
+		return np.concatenate(states, axis=0)
 
+	# returns all test pairs
 	def initial_test(self):
-		return self.test_pairs
+		states = np.array(self.test_data.keys())
+		empty_states = np.array([self.params.num_node] * len(states))
+		return np.stack([states, empty_states], axis=1)
 
 	def next_state(self, starts, actions):
 		nexts = []
@@ -66,32 +81,15 @@ class Environment(object):
 				indices_copy.append(np.random.choice(index, max_length, replace=False))
 		return np.array(indices_copy)
 
-	def reward_multiprocessing(self, states, actions):
-		def worker(worker_id):
-			for idx, state, action in zip(range(len(states)), states, actions):
-				if idx % num_process == worker_id:
-					queue.put((state, action, np.array(self.trajectory_reward(state, action))))
-
+	def compute_reward(self, states, actions):
 		assert len(states) == len(actions)
-		num_process = self.params.num_process
-		queue = Queue()
-		processes = []
-		for id in range(num_process):
-			process = Process(target=worker, args=(id,))
-			process.start()
-			processes.append(process)
+		return_states, return_actions, return_rewards = [], [], []
+		for state, action in zip(states, actions):
+			return_states.append(state)
+			return_actions.append(action)
+			return_rewards.append(np.array(self.trajectory_reward(state, action)))
+		return np.concatenate(return_states, axis=0), np.concatenate(return_actions, axis=0), np.concatenate(return_rewards, axis=0)
 
-		ret_states, ret_actions, ret_rewards = [], [], []
-		for i in range(self.params.batch_size):
-			state, action, reward = queue.get()
-			ret_states.append(state)
-			ret_actions.append(action)
-			ret_rewards.append(reward)
-
-		for process in processes:
-			process.join()
-
-		return np.concatenate(ret_states, axis=0), np.concatenate(ret_actions, axis=0), np.concatenate(ret_rewards, axis=0)
 
 	def trajectory_reward(self, states, actions):
 		rewards = []